ff583e7e6feff32c0ea398b903297829c3fbad76
islam <islam@C010422882.zipangu.cac.co.jp>
2026-04-15 15:28:50 +0900

    lesson1完了

--- Java100_questions_cac2nd/src/lesson01/challenge03/Patisserie.java
+++ Java100_questions_cac2nd/src/lesson01/challenge03/Patisserie.java
@@ -22,5 +22,12 @@
 package lesson01.challenge03;
 
 public class Patisserie {
+	public static void main(String[] args) {
+		int num = 30;
+		System.out.println("\n本日のおすすめです。\n");
+		System.out.println("シトロン      \\250・・・残り" + num + "個");
+		System.out.println("ショコラ　　　\\280・・・残り" + num + "個");
+		System.out.println("ピスターシュ　\\320・・・残り" + num + "個");
+	}
 
 }
---
+++ Java100_questions_cac2nd/src/lesson04/challenge06/Advertisement.java
@@ -0,0 +1,15 @@
+package lesson04.challenge06;
+
+public class Advertisement {
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		// TODO 自動生成されたメソッド・スタブ
+
+		System.out.println("\nマカロン店で一緒に働いてくれる仲間を募集！\n");
+		System.out.println("勤務時間\n9:00-15:30まで");
+		System.out.println("時給\n\\1300");
+		System.out.println("賄いあり、制服貸与");
+
+	}
+
+}
